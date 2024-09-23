7978e2a08859607c78b8ecff961b3e7c9a200338
Sam Eary <samwise342@gmail.com>
2024-09-23 09:39:19 -0400

    Add functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use console::{Key, Term};
+use dialoguer::Input;
 use std::io;
 
-const OPTS: &[(&str, &str)] = &[("'a'", "to add"), ("'d'", "to delete"), ("'Esc'", "to exit")];
-//TODO add handler
+const OPTS: &[(&str, &str)] = &[
+    ("'a'", "to add"),
+    ("'d'", "to delete"),
+    ("'Esc'", "to exit"),
+];
 //TODO database integration
 fn main() -> io::Result<()> {
     let _ = run_app();
@@ -13,73 +17,100 @@ fn bold_str(text: &str) -> String {
     format!("\x1b[1m{text}\x1b[0m")
 }
 
-fn render_list(items: &Vec<&str>, term: &Term) -> io::Result<()> {
-    for (i, item) in items.iter().enumerate() {
-        term.write_line(format!("[{i}] {item}").as_str())?;
+fn italic_str(text: &str) -> String {
+    format!("\x1b[3m{text}\x1b[0m")
+}
+
+fn render_list(items: &Vec<String>, term: &Term) -> io::Result<()> {
+    if items.len() > 0 {
+        for (i, item) in items.iter().enumerate() {
+            term.write_line(format!("[{i}] {item}").as_str())?;
+        }
+    } else {
+        term.write_line(italic_str("No items in the list. Please add one.").as_str())?;
     }
     Ok(())
 }
 
-fn handle_delete(item_list: &mut Vec<&str>, term: &Term) -> io::Result<()> {
-    loop {
-        term.write_line("\nEnter item number to delete:")?;
-        let key = term.read_key()?;
+fn handle_delete(item_list: &mut Vec<String>) -> io::Result<()> {
+    let index: usize = Input::new()
+        .with_prompt("\nEnter item number to delete")
+        .validate_with(|input: &usize| -> Result<(), &str> {
+            if *input < item_list.len() {
+                Ok(())
+            } else {
+                Err("\nInvalid input. Please enter a number within the range.")
+            }
+        })
+        .interact()
+        .unwrap();
+    item_list.remove(index);
+    Ok(())
+}
 
-        match key {
-            Key::Char(c) if c.is_digit(10) => {
-                let num = c.to_digit(10).unwrap();
-                let num = usize::try_from(num).unwrap();
-                if num < item_list.len() {
-                    let _ = item_list.remove(num);
-                    break;
-                } else {
-                    term.write_line("\nNumber out of bounds. Please try again.")?;
-                }
-            }
-            _ => {
-                term.write_line("\nInvalid input. Please enter a number.")?;
-            }
-        }
-    }
+fn handle_add(item_list: &mut Vec<String>) -> io::Result<()> {
+    let item: String = Input::new()
+        .with_prompt("\nEnter a new item")
+        .interact_text()
+        .unwrap();
+    item_list.push(item);
     Ok(())
 }
 
 fn render_and_prompt(
-    item_list: &Vec<&str>,
+    item_list: &Vec<String>,
     term: &Term,
     app_title: &String,
-    app_opts: &String,
+    app_opts: String,
 ) -> io::Result<()> {
     term.clear_screen()?;
     term.write_line(app_title)?;
     render_list(item_list, term)?;
-    term.write_line(format!("\nPress any key. {app_opts}").as_str())?;
+    term.write_line(format!("\nPress any key: {app_opts}").as_str())?;
     Ok(())
 }
 
-fn run_app() -> io::Result<()> {
-    let mut item_list = vec!["foo", "bar", "baz", "qux"];
-    let term = Term::stdout();
-    let joined_opts = OPTS
-        .iter()
+fn gen_opts(empty_list: bool) -> String {
+    OPTS.iter()
+        .filter(|&&(key, _)| !(empty_list && key == "'d'"))
         .map(|&(key, description)| format!("{} {}", bold_str(key), description))
         .collect::<Vec<String>>()
-        .join(", ");
+        .join(", ")
+}
+
+fn run_app() -> io::Result<()> {
+    let mut item_list: Vec<String> = vec![];
+    let term = Term::stdout();
     let app_title = bold_str("Rust Todo\n");
-    render_and_prompt(&item_list, &term, &app_title, &joined_opts)?;
+    render_and_prompt(
+        &item_list,
+        &term,
+        &app_title,
+        gen_opts(item_list.is_empty()),
+    )?;
     loop {
         let key = term.read_key()?;
         match key {
             Key::Escape => {
                 break;
             }
+            Key::Char('a') => {
+                handle_add(&mut item_list)?;
+            }
             Key::Char('d') => {
                 // Pass a mutable reference of `item_list` to `handle_delete`
-                handle_delete(&mut item_list, &term)?;
+                if item_list.len() > 0 {
+                    handle_delete(&mut item_list)?;
+                }
             }
             _ => {}
         }
-        render_and_prompt(&item_list, &term, &app_title, &joined_opts)?;
+        render_and_prompt(
+            &item_list,
+            &term,
+            &app_title,
+            gen_opts(item_list.is_empty()),
+        )?;
     }
     Ok(())
 }
